e9cf1f07957e56c76772a9a35b8f3ad49da09ac7
Ronen Lapushner <ronenk17@gmail.com>
2022-07-29 18:00:36 +0300

    Removed SUI

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
-  import List, { Item, Text } from '@smui/list';
+  type RequestItem = {
+    title: string;
+    url: string;
+  };
+
+  let requests: Array<RequestItem> = [];
+  let searchTerm = '';
 </script>
 
-<div class="bg-gray-700 h-full">
-  <List>
-    <Item><Text>LA</Text></Item>
-  </List>
+<div class="bg-slate-300 h-full">
+  <div class="flex flex-row p-2 bottom-2 border-b-blue-600 border-b-1 w-full">
+    <input bind:value={searchTerm} />
+  </div>
+
+  {#if requests.length !== 0}
+    <div>LIST HERE</div>
+  {:else}
+    <div class="flex justify-center items-center h-full">LA</div>
+  {/if}
 </div>
